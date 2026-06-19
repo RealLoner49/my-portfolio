b8ea52442118d7d0ac3b55943792d7ae1c9f12cf
RealLoner49 <ogbuefivictor076@gmail.com>
2026-06-19 22:51:14 +0100

    Update about avatar image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -71,7 +71,7 @@ const About = () => {
           transition={{ duration: 0.85, ease: "easeOut" }}
         >
           <div className="image-ring"></div>
-          <img src="/developer-avatar.png" alt="Developer avatar of Victor Ogbuefi" />
+          <img src="/VICTOR.png" alt="Developer avatar of Victor Ogbuefi" />
 
           <div className="about-badge badge-one">Full-stack</div>
           <div className="about-badge badge-two">Premium UI</div>
